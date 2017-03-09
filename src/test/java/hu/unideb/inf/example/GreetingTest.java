caae8fd2ecfe59aa9716309f7c599bc14f18b8a8
Szabó Máté <Szab Mt>
2017-03-09 22:10:04 +0100

    jacoco configuration added

---
+++ src/test/java/hu/unideb/inf/example/GreetingTest.java
@@ -0,0 +1,29 @@
+package hu.unideb.inf.example;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mates on 2017. 03. 09..
+ */
+public class GreetingTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getId() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getContent() throws Exception {
+
+    }
+
+}
